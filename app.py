cd81446447c71dba11ecc6fc74a1596ad322efb6
adwaithhr <128885469+adwaithhr@users.noreply.github.com>
2024-04-04 08:00:34 +0530

    program update

---
+++ app.py
@@ -0,0 +1,34 @@
+import tumour
+import gemini_tumour as geminiTumour
+import gemini_dementia as geminiDementia
+import gemini_pneumo as geminiPneumo
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
+
+dementia_text = str(geminiDementia.run('alzheimer_1.jpeg'))
+
+tumour_text = str(geminiTumour.run('ytumour.jpg'))
+
+pneumo_text = str(geminiPneumo.run('xray_pic2.jpg'))
+
+def generate_pdf(text, filename):
+    # Create a canvas
+    c = canvas.Canvas(filename, pagesize=letter)
+    
+    # Set font and size
+    c.setFont("Helvetica", 12)
+    
+    # Write text to the canvas
+    text_lines = text.split("\n")
+    y_coordinate = 750  # Starting y-coordinate
+    for line in text_lines:
+        c.drawString(50, y_coordinate, line)
+        y_coordinate -= 20  # Decrease y-coordinate for next line
+    
+    # Save the canvas to a PDF file
+    c.save()
+
+generate_pdf(dementia_text,"dementia_report.pdf")
+generate_pdf(tumour_text,"tumour_report.pdf")
+generate_pdf(pneumo_text,"pneumo_report.pdf")
---
+++ gemini_dementia.py
@@ -0,0 +1,19 @@
+import google.generativeai as genai
+import PIL.Image
+import dementia
+
+img = PIL.Image.open('ytumour.jpg')
+genai.configure(api_key='AIzaSyAQVTwJz8HE2A8br7T9FlNnh1YtntR1F-g')
+img_path = 'alzheimer_1.jpeg'
+
+
+def run(img_path):
+    img = PIL.Image.open(img_path)
+    val = dementia.run(img_path)
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([f"You are an advanced brain MRI radiologist assistant, experienced in observing and analysing a given MRI or CT scan image.The image contained is diagnosed with {val}. Return a short desciription of the image and how did you detect them (possible signs of disease present). Provide few checkpoints so that radiologists can cross check using them if they ever want to.Dont show any unecessary texts and cautions. ",img ])
+
+    # print(response.text)
+    return response.text
+
+run(img_path)
